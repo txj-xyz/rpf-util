f3563183c1a94713b4977247d444f0e0c20b519a
txj <git@txj69.xyz>
2021-04-28 11:48:56 -0600

    dev: rewrite logic to allow `cars` param for StoneR

--- convert.js
+++ convert.js
@@ -17,41 +17,68 @@ const getFiles = async function(dir) {
 }
 
 const GTAUtil = function(inputPath, outputPath){
-  const outputsubFolder = `${outputPath}\\${inputPath.split('\\')[inputPath.split('\\').length - 2]}\\${inputPath.match(/[^\\]*\.(\w+)$/g).toString()}\\`
-  // console.log(`${outputPath}\\${inputPath.split('\\')[inputPath.split('\\').length - 2]}\\${inputPath.match(/[^\\]*\.(\w+)$/g).toString()}\\`)
-  exec(`./tools/GTAUtil.exe`, ['extractarchive',`-i`,[inputPath],'-o',[outputsubFolder] ], function(err, data, pgmerr) {  
-    if(err) {
-      return console.log(err ?? "", data, pgmerr)
-    } else {
-      console.log(err ?? "", data, pgmerr)
-    }
-  })
+  console.log('[DEBUG] Util function inputPath:', inputPath);
+  console.log('[DEBUG] Util function outputPath:', outputPath);
+  const extractFolderConvert = `${outputPath}${inputPath.split('\\')[inputPath.split('\\').length - 2]}\\${inputPath.match(/[^\\]*\.(\w+)$/g).toString()}\\`
+  const carsExtractFolderConvert = `${outputPath}${inputPath.split('\\')[inputPath.split('\\').length - 2]}\\`
+  console.log('[DEBUG] Util function 1:', extractFolderConvert);
+  console.log('[DEBUG] Util function 2:', carsExtractFolderConvert);
+  // exec(`./tools/GTAUtil.exe`, ['extractarchive',`-i`,[inputPath],'-o',[extractFolderConvert] ], function(err, data, pgmerr) {  
+  //   if(err) {
+  //     return console.log(err ?? "", data, pgmerr)
+  //   } else {
+  //     console.log(err ?? "", data, pgmerr)
+  //   }
+  // })
 }
 
-if(!process.argv) {
-  return console.log('node .\\convert.js .\\inputFolderDirectPath\\ .\\outputFolderDirectPath\\\n\nIf you do not provide an output folder then the program will make a folder named `rpf-extract`')
-}
-if(!process.argv[2]){
-  return console.log('node .\\convert.js .\\inputFolderDirectPath\\ .\\outputFolderDirectPath\\\n\nIf you do not provide an output folder then the program will make a folder named `rpf-extract`')
+console.log('[DEBUG] ARGS:',process.argv)
+
+if(process.argv[2] === 'cars'){
+  // Set the output folder here and make the folder if not exist
+  if(!process.argv[4]){
+    outputFolder = '.\\rpf-extract'
+    fs.access(outputFolder, function(error) {
+      if (error) {
+        fs.mkdirSync('rpf-extract')
+        outputFolder = '.\\rpf-extract';
+      }
+    })
+  } else { outputFolder = process.argv[4]; }
+
+  getFiles(process.argv[3]).then(allFiles => {
+    allFiles.forEach(file => {
+        if(file.endsWith('.rpf'))
+        GTAUtil(file, outputFolder)
+    });
+  })
+} else if(process.argv[2].includes('.\\')){
+  if(!process.argv[3]){
+    outputFolder = '.\\rpf-extract'
+    fs.access(outputFolder, function(error) {
+      if (error) {
+        fs.mkdirSync('rpf-extract')
+        outputFolder = '.\\rpf-extract';
+      }
+    })
+  } else { outputFolder = process.argv[3]; }
+  getFiles(process.argv[2]).then(allFiles => {
+    allFiles.forEach(file => {
+        if(file.endsWith('.rpf'))
+        GTAUtil(file, outputFolder)
+    });
+  })
+} else {
+  return console.log('Usage: node .\\convert.js [cars] .\\inputFolderDirectPath\\ [.\\outputFolderDirectPath\\]')
 }
 
-if(!process.argv[3]){
-  outputFolder = '.\\rpf-extract'
-  fs.access(outputFolder, function(error) {
-    if (error) {
-      fs.mkdirSync('rpf-extract')
-      outputFolder = '.\\rpf-extract';
-    }
-  })
-} else { outputFolder = process.argv[3]; }
-// console.log(process.argv)
-getFiles(process.argv[2]).then(allFiles => {
-  convertedFileCount = 0
-  allFiles.forEach(file => {
-      if(file.endsWith('.rpf'))
-      GTAUtil(file, outputFolder)
-      console.log(`Done, Converted ${convertedFileCount} files.`);
-      // console.log(file.match(/[^\\]*\.(\w+)$/g).toString());
-      // file.match(/[^\\]*\.(\w+)$/g).toString().slice(0, -4)
-  });
-})
+// Set the output folder here and make the folder if not exist
+// if(!process.argv[3]){
+//   outputFolder = '.\\rpf-extract'
+//   fs.access(outputFolder, function(error) {
+//     if (error) {
+//       fs.mkdirSync('rpf-extract')
+//       outputFolder = '.\\rpf-extract';
+//     }
+//   })
+// } else { outputFolder = process.argv[3]; }
